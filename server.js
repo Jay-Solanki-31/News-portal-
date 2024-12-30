774f316a33cb4aba632a19a1bb055dfbc0c1f69d
Jay Solanki <jaysolanki1733@gmail.com>
2024-12-30 17:14:15 +0530

    add middleware for session management and flash messages; create post and user models

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes.js';
 import connectMongoDB from './db.js';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import flash from 'connect-flash';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -8,6 +11,34 @@ const PORT = process.env.PORT || 8000;
 
 // connect mongodb databse
 connectMongoDB()
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// cookie middleware
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// sessiom middleware
+app.use(session({
+    secret:process.env.COOKIE_SECRET,
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        maxAge: 60000*60*24*7 // 1week
+    }
+}));
+
+
+// flase messsage middleware
+app.use(flash())
+
+// store flash message for a views
+app.use(function(req,res,next){
+    res.locals.message = req.flash();
+    next();
+});       
+
 
 // set template engine
 app.set('view engine','ejs');
