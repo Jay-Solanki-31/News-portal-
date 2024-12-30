1a99c27d5cf9016cf63c80777646ebff4b172698
Jay Solanki <jaysolanki1733@gmail.com>
2024-12-30 15:41:16 +0530

    add views & partials

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ const PORT = process.env.PORT || 8000;
 
 // connect mongodb databse
 connectMongoDB()
+
+// set template engine
+app.set('view engine','ejs');
+// home route
+
 app.get('/',(req,res)=>{
-    res.send('Hello word');
+    res.render('index');
 });
 
 app.listen(PORT,()=>{
